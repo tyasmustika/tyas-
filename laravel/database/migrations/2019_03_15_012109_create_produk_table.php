a6b76bc419255386b06ba937637d0091946a1bda
tyasmustika <tyasmw06@gmail.com>
2019-03-15 10:50:18 +0700

    laravel

---
+++ laravel/database/migrations/2019_03_15_012109_create_produk_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdukTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama',50);
+			$table->integer('id_kategori');
+			$table->integer('qty');
+			$table->integer('harga_beli');
+			$table->integer('harga_jual');
+			$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produk');
+    }
+}
