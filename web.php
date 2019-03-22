d7b4fe66fb8fbd4683b4d36fd8d66013a1716caf
tyasmustika <tyasmw06@gmail.com>
2019-03-22 09:39:29 +0700

    routing

--- web.php
+++ web.php
@@ -14,6 +14,30 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function () {
-    return ('Hello World');
+
+Route::get('/welcome', function(){
+	echo "welcome";
 });
+
+Route::get('/show/{id}', function ($id) {
+	echo "Nilai Parameter adalah ".$id;
+});
+
+Route::get('/show/{id?}', function ($id=1) {
+	echo "Nilai Parameter adalah " .$id;
+});
+
+Route::get('/edit/{nama}', function ($nama) {
+	echo "Nilai Parameter adalah ".$nama;
+})->where('nama','[A-Za-z]+');
+
+Route::get('/index', function () {
+	echo "<a href='".route('create')."'>Akses Route dengan nama </a>";	
+});
+
+Route::get('/create', function () {
+	echo "route diakses menggunakan nama";
+})->name('create');
+
+Route::get('/barangs', 'barangsController@index');
+
